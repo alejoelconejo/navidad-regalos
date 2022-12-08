1078cca1fe80cb790a6aaa401bfe30a6b8c611a8
Alejo Janin <alejojanin@gmail.com>
2022-12-08 08:31:54 -0300

    Fix music playing

--- src/components/MusicButton/MusicButton.tsx
+++ src/components/MusicButton/MusicButton.tsx
@@ -1,22 +1,34 @@
 import { useState } from 'react'
 import musicMutedLogo from '../../images/musicMuted.svg'
+import musicPlay from '../../images/play.svg'
+import musicPause from '../../images/pause.svg'
 import musicLogo from '../../images/music.svg'
 import './MusicButton.css'
 
 const audio = new Audio('/christmas.mp3')
-audio.muted = true
 audio.loop = true
-audio.autoplay = true
 audio.volume = 0.3
 
 export const MusicButton = () => {
-  const [isAudioMuted, toggleMute] = useState(true)
+  const [isPlayingAudio, togglePlay] = useState(false)
 
-  audio.muted = isAudioMuted
+  const playMusic = () => {
+    if (isPlayingAudio) {
+      audio.pause()
+    } else {
+      audio.play()
+    }
+    togglePlay(!isPlayingAudio)
+  }
 
   return (
-    <button aria-label='Music' onClick={() => toggleMute(!isAudioMuted)}>
+    <button aria-label='Music' onClick={playMusic}>
       <img alt='music logo' src={audio.muted ? musicMutedLogo : musicLogo} />
+      <img
+        className='music-controls'
+        alt='music controls'
+        src={isPlayingAudio ? musicPause : musicPlay}
+      />
     </button>
   )
 }
